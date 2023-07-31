1cefdf4449f77615a0e1d5969d49b4b9e8f859aa
Simone999 <simonemele999@gmail.com>
2023-07-31 09:27:52 +0200

    Updates

--- src/losses.py
+++ src/losses.py
@@ -69,6 +69,52 @@ def generative_loss(logits, labels):
     return torch.mean(loss)
 
 
+# class RationaleLoss:
+#     def __init__(
+#         self,
+#         max_rationale_length,
+#         rationale_logits_name="rationale_logits",
+#         rationale_labels_name="rationale_labels",
+#         passage_mask_name="passage_mask",
+#     ) -> None:
+#         self.max_rationale_length = max_rationale_length
+#         self.rationale_logits_name = rationale_logits_name
+#         self.rationale_labels_name = rationale_labels_name
+#         self.passage_mask_name = passage_mask_name
+
+#     def __call__(self, outputs, targets: Dict[str, torch.Tensor]) -> torch.FloatTensor:
+#         rationale_logits = outputs[self.rationale_logits_name]
+#         rationale_labels = targets[self.rationale_labels_name]
+#         passage_mask = targets[self.passage_mask_name]
+
+#         rationale_labels = rationale_labels * passage_mask
+#         totals = torch.sum(passage_mask, -1, keepdim=True)
+#         positives = torch.sum(rationale_labels, -1, keepdim=True)
+#         negatives = totals - positives
+#         weight = torch.where(
+#             rationale_labels == 1.0, totals / positives, totals / negatives
+#         )
+#         weight = torch.where(weight != torch.inf, weight, 0.0)
+#         # weight = torch.ones_like(rationale_labels)
+
+#         per_token_loss = F.binary_cross_entropy_with_logits(
+#             input=rationale_logits,
+#             target=rationale_labels,
+#             weight=weight,
+#             reduction="none",
+#         )
+
+#         per_token_loss = per_token_loss * passage_mask
+#         per_sequence_loss = torch.sum(per_token_loss, dim=-1) / torch.sum(
+#             weight * passage_mask, dim=-1
+#         )
+
+#         rationale_lengths = torch.sum(rationale_labels, -1)
+#         valid_rationales = rationale_lengths <= self.max_rationale_length
+#         n_sequences = torch.sum(valid_rationales, dim=-1)
+
+#         return torch.sum(per_sequence_loss * valid_rationales, dim=-1) / n_sequences
+
 class RationaleLoss:
     def __init__(
         self,
@@ -83,37 +129,48 @@ class RationaleLoss:
         self.passage_mask_name = passage_mask_name
 
     def __call__(self, outputs, targets: Dict[str, torch.Tensor]) -> torch.FloatTensor:
+        """
+        li = w * BCE(y_pred_i, y_true_i)
+        , where w = w_positive if y_true_i is positive
+                w = w_negative if y_true_i is negative
+        w_positive = totals / positives
+        w_negative = totals / negatives
+        , where totals, positives and negatives are computed for each sequence
+
+        Ls = sum_i=1..seq_length li / sum(w_i)
+        L = sum_s=1..N ls / N,
+        , where N is the #sequences whose rationale length is <= max_rationale_length
+        """
+
         rationale_logits = outputs[self.rationale_logits_name]
         rationale_labels = targets[self.rationale_labels_name]
         passage_mask = targets[self.passage_mask_name]
 
-        totals = torch.sum(passage_mask, -1, keepdim=True)
         rationale_labels = rationale_labels * passage_mask
+
+        rationale_lengths = torch.sum(rationale_labels, dim=-1, keepdim=True)
+        valid_rationales = rationale_lengths <= self.max_rationale_length
+        n_sequences = torch.sum(valid_rationales)
+
+        totals = torch.sum(passage_mask, -1, keepdim=True)
         positives = torch.sum(rationale_labels, -1, keepdim=True)
         negatives = totals - positives
-        weight = torch.where(
+        weights = torch.where(
             rationale_labels == 1.0, totals / positives, totals / negatives
         )
-        weight = torch.where(weight != torch.inf, weight, 0.0)
-        # weight = torch.ones_like(rationale_labels)
+        weights = torch.where(weights != torch.inf, weights, 0.0)
+        weights = weights * passage_mask
+        weights = weights / torch.sum(weights, dim=-1, keepdim=True)
+        weights = weights * valid_rationales / n_sequences
 
-        per_token_loss = F.binary_cross_entropy_with_logits(
+        loss = F.binary_cross_entropy_with_logits(
             input=rationale_logits,
             target=rationale_labels,
-            weight=weight,
-            reduction="none",
+            weight=weights,
+            reduction="sum",
         )
-
-        per_token_loss = per_token_loss * passage_mask
-        per_sequence_loss = torch.sum(per_token_loss, dim=-1) / torch.sum(
-            weight * passage_mask, dim=-1
-        )
-
-        rationale_lengths = positives.squeeze(-1)
-        valid_rationales = rationale_lengths <= self.max_rationale_length
-        n_sequences = torch.sum(valid_rationales, dim=-1)
-
-        return torch.sum(per_sequence_loss * valid_rationales, dim=-1) / n_sequences
+        
+        return loss
 
 
 class EncoderDecoderRationaleLoss(RationaleLoss):
@@ -132,7 +189,7 @@ class EncoderRationaleLoss(ComputeLoss):
     def __call__(
         self, outputs, targets: Dict[str, torch.Tensor]
     ) -> Tuple[torch.FloatTensor, Dict[str, float]]:
-        return self.rationale_loss(outputs, targets), {}
+        return self.rationale_loss(outputs, targets) , {}
 
 
 # def composite_loss(*criteria: Criterion) -> ComputeLoss:
